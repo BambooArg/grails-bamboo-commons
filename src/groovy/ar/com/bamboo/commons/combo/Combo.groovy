92e6bc3adebea86ad516678e408e74fbe94d0bfc
orko <orquito@gmail.com>
2014-08-18 10:20:33 -0300

    Agrego clase combo

---
+++ src/groovy/ar/com/bamboo/commons/combo/Combo.groovy
@@ -0,0 +1,14 @@
+package ar.com.bamboo.commons.combo
+
+import groovy.transform.CompileStatic
+
+/**
+ * Created by orko on 18/08/14.
+ */
+@CompileStatic
+class Combo {
+
+    private String value
+    private String label
+
+}
