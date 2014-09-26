252399c0800f1bd32d5d904cf47b9a49e1119253
orko <orquito@gmail.com>
2014-09-25 23:57:47 -0300

    Agrego la clase BambooFile

---
+++ src/groovy/ar/com/bamboo/commons/file/BambooFile.groovy
@@ -0,0 +1,14 @@
+package ar.com.bamboo.commons.file
+
+import groovy.transform.CompileStatic
+
+/**
+ * Created by orko on 25/09/14.
+ */
+@CompileStatic
+class BambooFile {
+
+    String filename
+    byte[] filedata
+    String contentType
+}
