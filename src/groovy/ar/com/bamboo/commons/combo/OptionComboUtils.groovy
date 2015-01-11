227a0b7ccdb7a41e1c8cc824251bb72758b7e956
Orko <orquito@gmail.com>
2015-01-11 09:41:57 -0300

    Se agregan método a la clase combo utils para solucionar un problema en el proyecto de bighamlet con los combos de años y los años actuales

--- src/groovy/ar/com/bamboo/commons/combo/OptionComboUtils.groovy
+++ src/groovy/ar/com/bamboo/commons/combo/OptionComboUtils.groovy
@@ -21,7 +21,7 @@ class OptionComboUtils {
         return optionComboList
     }
 
-    public static List<OptionCombo> getComboMonthEspaniol(){
+    public static List<OptionCombo> getComboMonthSpanish(){
         List<OptionCombo> optionComboList = new ArrayList<OptionCombo>(12)
         Locale locate = new Locale("es")
         YearMonth md = null;
@@ -39,9 +39,39 @@ class OptionComboUtils {
        return getComboYear(currentYear)
     }
 
+    /**
+     * Devuelve una lista de OptionCombo de los últimos n (lastCountYear) años.
+     * Se inicia desde el año actual.
+     * Si lastCountYear es <= 0, setea un default de 3
+     * Si lastCountYear es >= 12, setea un límite de 12
+     * @param lastCountYear
+     * @return
+     */
+    public static List<OptionCombo> getComboYearLastN(int lastCountYear){
+        if (lastCountYear <= 0){
+            lastCountYear = 3
+        }else{
+            if (lastCountYear > 12){
+                lastCountYear = 12
+            }
+        }
+        DateTime time = new DateTime();
+        int currentYear = time.getYear()
+        return getComboYear(currentYear, lastCountYear)
+    }
+
     public static List<OptionCombo> getComboYear(int yearFrom){
         List<OptionCombo> optionComboList = new ArrayList<OptionCombo>(3)
         for (int i = yearFrom; i >= (yearFrom - 2); i--) {
+            optionComboList.add(new OptionCombo(value: String.valueOf(i), label: String.valueOf(i)))
+        }
+        return optionComboList
+    }
+
+    public static List<OptionCombo> getComboYear(int yearFrom, int countYear){
+        List<OptionCombo> optionComboList = new ArrayList<OptionCombo>(countYear)
+        int yearUntil = yearFrom - countYear
+        for (int i = yearFrom; i > yearUntil; i--) {
             optionComboList.add(new OptionCombo(value: String.valueOf(i), label: String.valueOf(i)))
         }
         return optionComboList
--- test/unit/ar/com/bamboo/commons/combo/OptionComboUtilsSpec.groovy
+++ test/unit/ar/com/bamboo/commons/combo/OptionComboUtilsSpec.groovy
@@ -31,7 +31,7 @@ class OptionComboUtilsSpec extends Specification {
     void "test getComboMonthEspaniol"() {
 
         when: "Llamo al método que pide los meses"
-        List<OptionCombo> optionComboList = OptionComboUtils.comboMonthEspaniol
+        List<OptionCombo> optionComboList = OptionComboUtils.comboMonthSpanish
         then: "Se genera un combo con todos los meses en formato texto"
         optionComboList
         optionComboList.size() == 12
@@ -64,5 +64,56 @@ class OptionComboUtilsSpec extends Specification {
         optionComboList.size() == 3
         optionComboList[0].value == String.valueOf(yearFrom)
         optionComboList[0].label == String.valueOf(yearFrom)
+    }
+
+    void "test getComboYear with parameter From and countyear"() {
+        given:
+        int yearFrom = 2012
+        int countYear = 6
+
+        when: "Llamo al método que pide los años con la cantidad de años que quiero = N"
+        List<OptionCombo> optionComboList = OptionComboUtils.getComboYear(yearFrom, countYear)
+
+        then: "Se genera un combo con el año actual + (N - 1) años anteriores"
+        optionComboList
+        optionComboList.size() == countYear
+        optionComboList[0].value == String.valueOf(yearFrom)
+        optionComboList[0].label == String.valueOf(yearFrom)
+        optionComboList[countYear - 1].value == String.valueOf(yearFrom - countYear + 1)
+        optionComboList[countYear - 1].label == String.valueOf(yearFrom - countYear + 1)
+    }
+
+    void "test getComboYearLastN"() {
+        given:
+        int countYear = 4
+        int lowCountYear = 0
+        int hightCountYear = 13
+        DateTime time = new DateTime();
+        int currentYear = time.getYear()
+
+        when: "llamo al método pidiendo los últimos N años dentro de las especificaciones, sin alterar el default"
+        List<OptionCombo> optionComboList = OptionComboUtils.getComboYearLastN(countYear)
+
+        then: "Se genera el combo de los últimos N años, desde la fecha actual"
+        optionComboList
+        optionComboList.size() == countYear
+        optionComboList[0].value == String.valueOf(currentYear)
+        optionComboList[0].label == String.valueOf(currentYear)
+        optionComboList[countYear - 1].value == String.valueOf(currentYear - countYear + 1)
+        optionComboList[countYear - 1].label == String.valueOf(currentYear - countYear + 1)
+
+        when: "Llamo al método con el parámetro lowCountYear = 0, por lo tanto tiene que dejar un mínimo default de 3"
+        optionComboList = OptionComboUtils.getComboYearLastN(lowCountYear)
+
+        then: "Se genera el combo de los últimos 3 años"
+        optionComboList
+        optionComboList.size() == 3
+
+        when: "Llamo al método con el parámetro hightCountYear = 13, por lo tanto tiene que dejar un máximo default de 12"
+        optionComboList = OptionComboUtils.getComboYearLastN(hightCountYear)
+
+        then: "Se genera el combo de los últimos 12 años"
+        optionComboList
+        optionComboList.size() == 12
     }
 }
