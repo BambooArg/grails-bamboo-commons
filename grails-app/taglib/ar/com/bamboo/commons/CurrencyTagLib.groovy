22bf8437383e0e278888c48764ddbd0fc182259a
Orko <orquito@gmail.com>
2015-02-13 07:14:03 -0300

    Agrego taglib para manejar el formato de moneda

---
+++ grails-app/taglib/ar/com/bamboo/commons/CurrencyTagLib.groovy
@@ -0,0 +1,24 @@
+package ar.com.bamboo.commons
+
+import ar.com.bamboo.commons.number.CurrencyUtils
+
+class CurrencyTagLib {
+    static defaultEncodeAs = [taglib:'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+
+    /**
+     * Imprime la hora en formato humano.
+     * Pasado un string del tipo HHMM devuelve HH:MM
+     *
+     * @attr price
+     * */
+    def formatPrice = {attrs ->
+        String output = ""
+        if (attrs.price == null){
+            output = "---"
+        }else{
+            output = CurrencyUtils.formatArgentinianPrice(attrs.price)
+        }
+        out << output
+    }
+}
