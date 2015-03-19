6bb32323336802fefbf1584e3cfed933b3863513
Orko <orquito@gmail.com>
2015-03-19 07:39:57 -0300

    Modifico el script de publish del proyecto

--- publish.sh
+++ publish.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 echo "instalando bamboo commons"
-grails clean && grails refresh-dependencies && grails publish-plugin
-
-#grails publish-plugin 
+grails clean && grails refresh-dependencies && grails maven-install
 
