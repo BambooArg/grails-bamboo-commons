fd467233465b79e2519f7d4962f618dab9a61651
Orko <orquito@gmail.com>
2015-05-23 18:29:16 -0300

    Cambio la versión a 1.0.6

--- GrailsBambooCommonsGrailsPlugin.groovy
+++ GrailsBambooCommonsGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class GrailsBambooCommonsGrailsPlugin {
     // the plugin version
-    def version = "1.0.5"
+    def version = "1.0.6"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
